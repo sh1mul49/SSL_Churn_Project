55417b857f72bd5810874c51cef86bf12e4fc38a
sh1mul49 <hstumahmud@gmail.com>
2024-11-25 13:20:05 +0600

    data and code

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,14 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging 
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass
+class DataIngestionConfig:
+    train_data_path: str=os.path.join('art')
+
